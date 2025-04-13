3f6df718b291b43224f6196666ff874a99999a55
soohwan lee <soohwanlee@soohwans-MacBook-Air.local>
2025-04-13 13:12:24 -0400

    Update the about page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,15 @@
 export const metadata = {
-    title: 'About | Next.js',
-  }
+  title: "About | Next.js",
+};
 
-export default function AboutUs(){
-    return <h1>About us!</h1>
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About us!</h1>
+      <p>
+        Welcome to the official explorer for The New York Times Best Seller list
+        explorer. We hope you enjoy your stay!
+      </p>
+    </div>
+  );
 }
